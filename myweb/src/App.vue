a5c4e992a0eef6d821b0bb1aa28607a279d0c381
ENZO1994 <120831767@qq.com>
2018-06-09 18:55:57 +0800

    full vue project

---
+++ myweb/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hr>
+    <button class = "btn btn-primary">确定</button>
+    <hr>
+
+      <hr>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ myweb/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+   <pre>{{content}}</pre>
+       <div class="myclass">你好</div>
+  </div>
+</template>
+
+<script>
+import '../assets/my.css'
+export default {
+  name: "HelloWorld",
+  data () {
+    return {
+      content: ""
+    }
+  },
+  mounted(){
+    this.axios.post("http://api.komavideo.com/news/list").then(res=>{
+      this.content = res.data
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
